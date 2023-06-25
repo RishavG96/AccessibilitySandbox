6a7fde8e0c3225d271b2630322de8c8eb3bc9be0
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-25 18:23:20 +0530

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Rishav Gupta on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
